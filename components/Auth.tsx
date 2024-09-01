39677d397d7dec4d3fedef8ea67efddc8044107b
Maria Eduarda Ferreira <maria.ferreira@feedz.com.br>
2024-09-01 22:29:58 +0900

    Setting lib expo-apple-authentication

---
+++ components/Auth.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Auth() {
+    return (
+        <View style={styles.container}>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
